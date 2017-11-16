df4214bca0705a9d9adf4b3e1fb8c1cda76acad9
Savason <Savason@bigmir.net>
2017-11-16 17:22:34 +0200

    Test for GL(Trainee)

---
+++ GL-Test/js/Task_4.js
@@ -0,0 +1,58 @@
+﻿//If we change any value in the model,
+//it dynamically changes to the html-view,
+//after the page reloads
+var model = {
+    skills: [
+        { name: "HTML5", level: 8 },
+        { name: "CSS3", level: 5 },
+        { name: "JavaScript", level: 10 },
+        { name: "ReactJS", level: 8 },
+        { name: "AngularJS", level: 8 },
+    ]
+};
+angular.module("skillsApp", [])
+    .controller("skillsCtrl", function ($scope) {
+
+        $scope.data = model;
+        var max_level = 10;
+        for (var i = 0; i < max_level; i++) {
+            if (i < model.skills[0].level) {
+                document.getElementsByClassName('html_level')[0].innerHTML += '<img src="img/skills_level.png" />';
+            }
+            else {
+                document.getElementsByClassName('html_level')[0].innerHTML += '<img src="img/skills_img.png" />';
+            }
+        }
+        for (var i = 0; i < max_level; i++) {
+            if (i < model.skills[1].level) {
+                document.getElementsByClassName('css_level')[0].innerHTML += '<img src="img/skills_level.png" />';
+            }
+            else {
+                document.getElementsByClassName('css_level')[0].innerHTML += '<img src="img/skills_img.png" />';
+            }
+        }
+        for (var i = 0; i < max_level; i++) {
+            if (i < model.skills[2].level) {
+                document.getElementsByClassName('js_level')[0].innerHTML += '<img src="img/skills_level.png" />';
+            }
+            else {
+                document.getElementsByClassName('js_level')[0].innerHTML += '<img src="img/skills_img.png" />';
+            }
+        }
+        for (var i = 0; i < max_level; i++) {
+            if (i < model.skills[3].level) {
+                document.getElementsByClassName('react_level')[0].innerHTML += '<img src="img/skills_level.png" />';
+            }
+            else {
+                document.getElementsByClassName('react_level')[0].innerHTML += '<img src="img/skills_img.png" />';
+            }
+        }
+        for (var i = 0; i < max_level; i++) {
+            if (i < model.skills[4].level) {
+                document.getElementsByClassName('angular_level')[0].innerHTML += '<img src="img/skills_level.png" />';
+            }
+            else {
+                document.getElementsByClassName('angular_level')[0].innerHTML += '<img src="img/skills_img.png" />';
+            }
+        }
+    });
---
+++ GL-Test/js/Task_5.js
@@ -0,0 +1,16 @@
+﻿window.addEventListener('load', function () {
+    //actions to form inputs which show in the browser
+    //console entered value.
+    var name_input = document.getElementsByClassName('name_input')[0];
+    var email_input = document.getElementsByClassName('email_input')[0];
+    var message_input = document.getElementsByClassName('message_input')[0];
+    name_input.addEventListener('blur', function () {
+        console.log("Value of name's input = " + name_input.value);
+    });
+    email_input.addEventListener('blur', function () {
+        console.log("Value of email's input = " + email_input.value);
+    });
+    message_input.addEventListener('blur', function () {
+        console.log("Value of message's input = " + message_input.value);
+    });
+});
